925f792de3c26b0ef61613d12d2f03aad860f11b
zekroTJA <riho@live.de>
2020-02-03 11:29:13 +0100

    [RestAPI] Add timestaps to log output

--- RestAPI/Program.cs
+++ RestAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace RestAPI
 {
@@ -12,6 +13,13 @@ namespace RestAPI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConsole(c =>
+                    {
+                        c.TimestampFormat = "yyyy/MM/dd - HH:mm:ss | ";
+                    });
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
