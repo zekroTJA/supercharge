e8a2503fe577c30712130ab0a6865ae74c1391d3
zekroTJA <riho@live.de>
2020-02-05 14:13:09 +0100

    [RAPIAL] add rate limit handling

--- RiotAPIAccessLayer/Requests.cs
+++ RiotAPIAccessLayer/Requests.cs
@@ -1,4 +1,7 @@
 ﻿using RiotAPIAccessLayer.Exceptions;
+using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -8,8 +11,11 @@ namespace RiotAPIAccessLayer
     class Requests
     {
         private const string ROOT_URI = "https://{0}.api.riotgames.com";
+        private const int haltBufferSecs = 1;
 
         private readonly HttpClient client = new HttpClient();
+
+        private DateTime haltUntil;
 
         public Requests(string apiToken)
         {
@@ -23,7 +29,24 @@ namespace RiotAPIAccessLayer
             string apiVersion, 
             string path)
         {
+            var now = DateTime.Now;
+            if (haltUntil > DateTime.Now)
+            {
+                await Task.Delay(haltUntil - now);
+                return await Get<T>(server, api, apiVersion, path);
+            }
+
             var res = await client.GetAsync($"{string.Format(ROOT_URI, server)}/{api}/{apiVersion}/{path}");
+
+            if (res.StatusCode == HttpStatusCode.TooManyRequests)
+            {
+                var waitSecs = int.Parse(res.Headers.GetValues("Retry-After").FirstOrDefault());
+                var wait = TimeSpan.FromSeconds(waitSecs + haltBufferSecs);
+                haltUntil = DateTime.Now.Add(wait);
+
+                await Task.Delay(wait);
+                return await Get<T>(server, api, apiVersion, path);
+            }
 
             if (!res.IsSuccessStatusCode)
             {
