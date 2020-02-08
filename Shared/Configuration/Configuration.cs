e7c4ef7d37068a84b346921766c4a014ae6f9e0b
zekro <private@zekro.de>
2020-02-08 11:15:12 +0100

    [Shared] add shared configuration parser

---
+++ Shared/Configuration/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Shared.Configuration
+{
+    public class Configuration
+    {
+        public static IConfiguration ParseConfig() =>
+            new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .AddEnvironmentVariables(prefix: "SC_")
+                .Build();
+    }
+}
